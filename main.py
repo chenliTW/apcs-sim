7c0d307f40c6364ab1b26e8fc2f2793cf8a76e51
ChenLi <ab0897867564534231@gmail.com>
2019-04-24 23:00:27 +0800

    噴了很多殺蟲器(Debug)
     - 讓回傳judge結果的function正常運作
     - 增加 chormedriver.exe

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sys
 
 from main_ui import Ui_MainWindow
-from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5 import QtTest
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -12,7 +14,7 @@ import webbrowser
 
 import json
 
-from time import sleep
+import time
 
 ###設定區###
 
@@ -38,7 +40,7 @@ def setup():
     _problem_id_1ist[2]=input('輸入第三題題號(EX. a001):')
     _problem_id_1ist[3]=input('輸入第四題題號(EX. a001):')
     print('請在稍後彈出的瀏覽器登入您的zerojudge帳號')
-    sleep(1)
+    time.sleep(1)
     login()
 
 def login():
@@ -80,13 +82,18 @@ def fn_submit_problem(__problem_id,__code):
     ,'Cookie': 'JSESSIONID='+_cookie
     }
     __data={'action':'SubmitCode','language':'CPP','code':__code,'contestid':'0','problemid':__problem_id}
-    #requests.post(_zg_submit_url,headers=__header,cookies={'JSESSIONID':_cookie},data=__data)
-    return fn_get_result(__problem_id)
+    requests.post(_zg_submit_url,headers=__header,cookies={'JSESSIONID':_cookie},data=__data)
 
 def fn_get_result(__problem_id):
     __result=requests.get(_zg_result_url+_username,cookies={'JSESSIONID':_cookie})
     __soup = BeautifulSoup(__result.text, "lxml")
-    __judge_id=__soup.find(id="solutionid").text
+    __judges=__soup.find_all(width="38%")
+    __judge_num=0
+    for i in __judges:
+        if(i.text.find(__problem_id)>=0):
+            break
+        __judge_num+=1
+    __judge_id=__soup.find_all(id="solutionid")[__judge_num*3].text
     __result=requests.get(_zg_judgement_url+__judge_id,cookies={'JSESSIONID':_cookie})
     __out=str()
     __num=int(1)
@@ -100,7 +107,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
         self.setWindowTitle("APCS實作題")
-        
+
         #按鈕->看題目
         self.view_problem_1.clicked.connect(self.fn_view_problem_1)
         self.view_problem_2.clicked.connect(self.fn_view_problem_2)
@@ -112,7 +119,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.choose_file_2.clicked.connect(self.fn_choose_file_2)
         self.choose_file_3.clicked.connect(self.fn_choose_file_3)
         self.choose_file_4.clicked.connect(self.fn_choose_file_4)
-        
+
     def fn_view_problem_1(self):
         fn_pop_problem(_problem_id_1ist[0])
     def fn_view_problem_2(self):
@@ -125,31 +132,59 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def fn_choose_file_1(self):
         _file_1 = QFileDialog.getOpenFileName(self,"開啟檔案","/","C++ files(*.cpp)")
         self.file_dir_1.setPlainText(_file_1[0].split("/")[-1])
-        self.view_program_1.setPlainText(open(_file_1[0], mode='r').read())
-        self.result_1.setPlainText('等待評分結果...')
-        _out=fn_submit_problem(_problem_id_1ist[0],open(_file_1[0], mode='r').read())
-        self.result_1.setPlainText(_out)
+        try:
+            self.view_program_1.setPlainText(open(_file_1[0], mode='r').read())
+            fn_submit_problem(_problem_id_1ist[0],open(_file_1[0], mode='r').read())
+            self.result_1.setPlainText('等待評分結果...')
+            for i in range(0,30,1):
+                QtTest.QTest.qWait(2000)
+                _recv=fn_get_result(_problem_id_1ist[0])
+                if(_recv!=''):
+                    self.result_1.setPlainText(_recv)
+        except:
+            print('upload file error')
     def fn_choose_file_2(self):
         _file_2 = QFileDialog.getOpenFileName(self,"開啟檔案","/","C++ files(*.cpp)")
         self.file_dir_2.setPlainText(_file_2[0].split("/")[-1])
-        self.view_program_2.setPlainText(open(_file_2[0], mode='r').read())
-        self.result_2.setPlainText('等待評分結果...')
-        _out=fn_submit_problem(_problem_id_1ist[1],open(_file_2[0], mode='r').read())
-        self.result_2.setPlainText(_out)
+        try:
+            self.view_program_2.setPlainText(open(_file_2[0], mode='r').read())
+            fn_submit_problem(_problem_id_1ist[1],open(_file_2[0], mode='r').read())
+            self.result_2.setPlainText('等待評分結果...')
+            for i in range(0,30,1):
+                QtTest.QTest.qWait(2000)
+                _recv=fn_get_result(_problem_id_1ist[1])
+                if(_recv!=''):
+                    self.result_2.setPlainText(_recv)
+        except:
+            print('upload file error')    
     def fn_choose_file_3(self):
         _file_3 = QFileDialog.getOpenFileName(self,"開啟檔案","/","C++ files(*.cpp)")
         self.file_dir_3.setPlainText(_file_3[0].split("/")[-1])
-        self.view_program_3.setPlainText(open(_file_3[0], mode='r').read())
-        self.result_3.setPlainText('等待評分結果...')
-        _out=fn_submit_problem(_problem_id_1ist[2],open(_file_3[0], mode='r').read())
-        self.result_3.setPlainText(_out)
+        try:
+            self.view_program_3.setPlainText(open(_file_3[0], mode='r').read())
+            fn_submit_problem(_problem_id_1ist[2],open(_file_3[0], mode='r').read())
+            self.result_3.setPlainText('等待評分結果...')
+            for i in range(0,30,1):
+                QtTest.QTest.qWait(2000)
+                _recv=fn_get_result(_problem_id_1ist[2])
+                if(_recv!=''):
+                    self.result_3.setPlainText(_recv)
+        except:
+            print('upload file error')  
     def fn_choose_file_4(self):
         _file_4 = QFileDialog.getOpenFileName(self,"開啟檔案","/","C++ files(*.cpp)")
         self.file_dir_4.setPlainText(_file_4[0].split("/")[-1])
-        self.view_program_4.setPlainText(open(_file_4[0], mode='r').read())
-        self.result_4.setPlainText('等待評分結果...')
-        _out=fn_submit_problem(_problem_id_1ist[3],open(_file_4[0], mode='r').read())
-        self.result_4.setPlainText(_out)
+        try:
+            self.view_program_4.setPlainText(open(_file_4[0], mode='r').read())
+            fn_submit_problem(_problem_id_1ist[3],open(_file_4[0], mode='r').read())
+            self.result_4.setPlainText('等待評分結果...')
+            for i in range(0,30,1):
+                QtTest.QTest.qWait(2000)
+                _recv=fn_get_result(_problem_id_1ist[3])
+                if(_recv!=''):
+                    self.result_4.setPlainText(_recv)
+        except:
+            print('upload file error')  
 
 if __name__ == "__main__":
     setup()
